e9640ea30da09def0984fb6228777abeae126fcc
Evgeniy Zoloedov <evgenijzoloedov@gmail.com>
2025-12-09 19:17:38 +0300

    WIP

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
 	cors({
 		origin: '*', // Allow all origins
-		credentials: true,
+		credentials: false,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	})
